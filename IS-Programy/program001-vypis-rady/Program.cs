680197f62b4a372575410e8293582cada4878204
Adam Erben <adam.erben@seznam.cz>
2025-10-02 14:06:19 +0200

    #3 Výpis řady - vstup pro první číslo

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,30 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("*********Výpis číselné řady****************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("************Adam Erben*********************");
+    Console.WriteLine("*************2.10.2025*********************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale ŠPATNĚ řešený
+    // Console.Write("Zadejte první číslo řady: ");
+    // int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, ale LÉPE řešený
+    Console.Write("Zadejte první číslo řady (musí být celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Zadejte první číslo řady (musí být celé číslo): ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte 'a'");
+    again = Console.ReadLine();
+}
