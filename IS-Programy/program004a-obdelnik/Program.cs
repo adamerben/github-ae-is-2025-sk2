79dfa9e884cf0ad9511e70de985ed040373ddc5c
Adam Erben <adam.erben@seznam.cz>
2025-10-23 14:06:35 +0200

    #6 obdelnik

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,42 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Tomáš Žižka ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte šířku obdelníku: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obdelníku znovu: ");
+    }
+
+    Console.Write("Zadejte výšku obdelníku: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obdelníku znovu: ");
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write("* ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
