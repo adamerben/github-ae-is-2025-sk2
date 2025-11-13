39b0ffdde064cadc14120ee2e0f730ca5a1ee9c2
Adam Erben <adam.erben@seznam.cz>
2025-11-13 13:12:52 +0100

    #8 max, min, posMax, posMin

---
+++ IS-Programy/program006a-max-min/Program.cs
@@ -0,0 +1,89 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Generátor **********************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Adam Erben ******************");
+    Console.WriteLine("************** 13.11.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu: ");
+    }
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound;
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
+    }
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound;
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("===============================================");
+    Console.WriteLine($"počet čísel: {n}, dolní mez: {lowerBound}, horní mez: {upperBound}");
+    Console.WriteLine("===============================================");
+    Console.WriteLine();
+
+    // deklarace pole (array)
+
+    int[] myRandNumbs = new int[n];
+
+    Random myRandNumb = new Random();
+
+    Console.WriteLine();
+    Console.WriteLine("Nahodná čísla:");
+    for (int i = 0; i < n; i++)
+    {
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound + 1);
+        Console.Write($"{myRandNumbs[i]} ");
+    }
+
+
+    // hledaní maxima, pozice maxima, minima, pozice minima
+
+    int max = myRandNumbs[0];
+    int min = myRandNumbs[0];
+    int posMax = 0;
+    int posMin = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (myRandNumbs[i] > max)
+        {
+            max = myRandNumbs[i];
+            posMax = i;
+        }
+        if (myRandNumbs[i] < min)
+        {
+            min = myRandNumbs[i];
+            posMin = i;
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("======================================");
+    Console.WriteLine($"Maximum: {max}, pozice maxima: {posMax}");
+    Console.WriteLine($"Minimum: {min}, pozice minima: {posMin}");
+    Console.WriteLine("======================================");
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
