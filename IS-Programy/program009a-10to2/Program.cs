d6e2d00efc0337aca4f1385fcb8fd52adc76a572
Adam Erben <adam.erben@seznam.cz>
2025-12-04 13:36:02 +0100

    #11 prevod z 10 do 2 soustavy

---
+++ IS-Programy/program009a-10to2/Program.cs
@@ -0,0 +1,37 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Tomáš Žižka ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte desítkové číslo (přirozené číslo): ");
+    uint number10;
+    while (!uint.TryParse(Console.ReadLine(), out number10))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    uint[] myArray = new uint[32];
+    uint backupNumber10 = number10;
+    uint zbytek;
+
+    int i;
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
